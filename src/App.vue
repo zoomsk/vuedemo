aa06aa57a52ad950f9b64b59eee9719f76639fb9
zoomsk <ghghqa@126.com>
2020-04-01 10:54:30 +0800

    基本vue-cli3创建 ,完成stylus、base css设置 @通用目录设置

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+    <Index/>
+  </div>
+</template>
+
+<script>
+import Index from '@/page/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Index
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import "~@/assets/styles/base/normalize.css";
+  @import "~@/assets/styles/base/base.css";
+  @import "~@/assets/styles/base/variable.styl";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $text-color-6;
+
+}
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>index</h1>
+        <div>this is template body</div>
+
+        <div class="flex">
+             <h2>1</h2>
+             <h2>2</h2>
+             <h2>3</h2>
+            <h3>
+                <img src="@/assets/images/logo.png" alt="logo">
+                <img :src="logoUrl" alt="logo">
+            </h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue',
+                logoUrl: require('@/assets/images/logo.png')
+            }
+        },
+    }
+</script>
+
+<style lang="stylus">
+    @import "~@/assets/styles/base/variable.styl";
+
+
+
+</style>
